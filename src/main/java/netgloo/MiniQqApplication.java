cee18f3687b8393cc8edc0d0befba236cd8fb808
liukui <liukui@wisdomin.com>
2017-03-29 21:37:53 +0800

    初始化

---
+++ src/main/java/netgloo/MiniQqApplication.java
@@ -0,0 +1,12 @@
+package netgloo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniQqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniQqApplication.class, args);
+	}
+}
---
+++ src/test/java/netgloo/MiniQqApplicationTests.java
@@ -0,0 +1,18 @@
+package netgloo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MiniQqApplication.class)
+@WebAppConfiguration
+public class MiniQqApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
